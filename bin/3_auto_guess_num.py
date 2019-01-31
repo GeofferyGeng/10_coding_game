64f8d86b1e8ddc9b5a2bad1db06ab73a63fee723
GeofferyGeng <>
2019-01-31 15:06:18 +0800

    finish 3

---
+++ bin/3_auto_guess_num.py
@@ -0,0 +1,29 @@
+import random
+import math
+
+
+def main():
+    b = 100
+    c = 1000
+    a = round((b + c) / 2)
+    y = random.randint(b, c)
+    i = 0
+    while True:
+        i = i + 1
+        print(i)
+        print(a)
+        print(y)
+        if a > y:
+            c = a
+            a = math.floor((c + b) / 2)
+        elif a < y:
+            b = a
+            a = math.ceil((c + b) / 2)
+        else:
+            print("恭喜您猜对啦！")
+            break
+    print("经过 ", i, " 次猜对，猜中数字为：", y)
+
+
+if __name__ == '__main__':
+    main()
