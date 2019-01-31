0c275e45c87542a80eaa13df4f1889cde8bec87d
GeofferyGeng <>
2019-01-31 15:26:53 +0800

    finish 3

--- bin/3_auto_guess_num.py
+++ bin/3_auto_guess_num.py
@@ -2,9 +2,10 @@ import random
 import math
 
 
-def main():
+# 二分法逼近
+def method_1():
     b = 100
-    c = 10000
+    c = 1000
     a = round((b + c) / 2)
     y = random.randint(b, c)
     i = 0
@@ -20,6 +21,19 @@ def main():
             print("恭喜您猜对啦！")
             break
     print("经过 ", i, " 次猜对，猜中数字为：", y)
+
+
+# 暂时没有找到更好的算法
+def method_2():
+    b = 100
+    c = 1000
+    y = random.randint(b, c)
+    i = 0
+
+
+
+def main():
+    method_2()
 
 
 if __name__ == '__main__':
